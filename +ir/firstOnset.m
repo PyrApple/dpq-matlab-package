fa62a1357fc71ad26cdf1b5fce514c6c42e1362a
David Poirier-Quinot <davipoir@gmail.com>
2021-10-05 23:02:46 +0200

    +ir first/last onset: added index of ir in matrix where id is found as output

--- +ir/firstOnset.m
+++ +ir/firstOnset.m
@@ -1,27 +1,36 @@
-function first_onset_n = firstOnset(ir_m, thresh)
+function [onsetIdSamp, onsetIdIr] = firstOnset(ir_m, thresh)
 
 % Detects first onset above thresh in matrix or impulse responses
 %
-% first_onset_n = firstOnset(ir_m, thresh)
+% [onsetIdSamp, onsetIdIr] = firstOnset(ir_m, thresh)
 %
 % ir_m is a matrix of impulse responses, each column an ir, thresh is the
-% onset relative (to max ir value) threshold
+% onset relative (to max ir value) threshold. onsetIdSamp is the index of
+% the first onset in the ir, onsetIdIr is the id of the ir in the matrix
+% ir_m.
 
 % init default arguments
 if( nargin < 2 ); thresh = 1e-3; end
 
+% handle vector input
+if( isrow(ir_m) ); ir_m = ir_m.'; end
+    
 % init locals
-first_onset_n = Inf;
+onsetIdSamp = Inf;
+onsetIdIr = 0;
 
 % loop over irs
-for i = 1:size(ir_m,2)
+for iIr = 1:size(ir_m,2)
     
     % find first onset
-    ir_v = abs(ir_m(:,i));
-    last = find(ir_v > max(ir_v) * thresh, 1, 'first');
+    ir_v = abs(ir_m(:,iIr));
+    id = find(ir_v > max(ir_v) * thresh, 1, 'first');
     
     % save to locals
-    first_onset_n = min(first_onset_n, last);
+    if( id < onsetIdSamp )
+        onsetIdSamp = id;
+        onsetIdIr = iIr;
+    end
     
 end
 
@@ -34,5 +43,21 @@ return
 filePath = '/Users/pyrus/SharedData/HRTFs/listen_hrir_subset/raw/sofa/listen_irc_1008.sofa';
 s = SOFAload(filePath);
 
+% shape data
 ir_m = squeeze(s.Data.IR(:, 1, :)).';
-idStart = dpq.ir.firstOnset(ir_m, .5e-1)
+% ir_m = squeeze(s.Data.IR(:, 1, :)).';
+
+% test function
+[idStart, idIr] = dpq.ir.firstOnset(ir_m, 0.5);
+
+% plot
+plot(ir_m, 'Color', 0.8*[1 1 1]);
+hold on,
+plot(ir_m(:, idIr), '-r');
+line([idStart idStart], [2*min(min(ir_m)), 2*max(max(ir_m))], 'Color', 'r', 'LineStyle', '--');
+
+% format
+hold off, 
+grid on, grid minor
+
+
--- +ir/lastOnset.m
+++ +ir/lastOnset.m
@@ -1,4 +1,4 @@
-function [last_onset_n] = lastOnset(ir_m, thresh)
+function [onsetIdSamp, onsetIdIr] = lastOnset(ir_m, thresh)
 
 % Detects last onset above thresh in matrix or impulse responses
 %
@@ -10,19 +10,26 @@ function [last_onset_n] = lastOnset(ir_m, thresh)
 % init default arguments
 if( nargin < 2 ); thresh = 1e-3; end
 
+% handle vector input
+if( isrow(ir_m) ); ir_m = ir_m.'; end
+    
 % init locals
-last_onset_n = -Inf;
+onsetIdSamp = -Inf;
+onsetIdIr = 0;
 
 % loop over irs
-for i = 1:size(ir_m,2)
+for iIr = 1:size(ir_m,2)
     
     % find last onset
-    ir_v = abs(ir_m(:,i));
+    ir_v = abs(ir_m(:,iIr));
     last = find(ir_v > max(ir_v) * thresh, 1, 'last');
     
     % save to locals
-    last_onset_n = max(last_onset_n, last);
-    
+    if( last > onsetIdSamp )
+        onsetIdSamp = last;
+        onsetIdIr = iIr;
+    end
+        
 end
 
 return 
@@ -34,5 +41,19 @@ return
 filePath = '/Users/pyrus/SharedData/HRTFs/listen_hrir_subset/raw/sofa/listen_irc_1008.sofa';
 s = SOFAload(filePath);
 
+% shape data
 ir_m = squeeze(s.Data.IR(:, 1, :)).';
-idStop = dpq.ir.lastOnset(ir_m, .5e-1)
+% ir_m = squeeze(s.Data.IR(:, 1, :)).';
+
+% test function
+[idEnd, idIr] = dpq.ir.lastOnset(ir_m, 0.1);
+
+% plot
+plot(ir_m, 'Color', 0.8*[1 1 1]);
+hold on,
+plot(ir_m(:, idIr), '-r');
+line([idEnd idEnd], [2*min(min(ir_m)), 2*max(max(ir_m))], 'Color', 'r', 'LineStyle', '--');
+
+% format
+hold off, 
+grid on, grid minor
